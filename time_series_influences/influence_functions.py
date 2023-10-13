00df8998690efab303f3a275e5546d98c8cee508
Yizi Zhang <yz4123@columbia.edu>
2023-10-13 18:47:26 -0400

    correction

--- time_series_influences/influence_functions.py
+++ time_series_influences/influence_functions.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-def empirical_IC_linear_approx(x, y, X, beta, b):
+def empirical_IC_linear_approx(x, y, X, beta, b, inv_hess):
     """
     Compute empirical influence curves for model parameters with linear approximation.
     No leave-one-out retraining is needed. 
     """
-    eic = (1/len(X)) * np.linalg.inv(X.T @ X) @ (x * (y - x.T @ beta - b))
+    eic = (1/len(X)) * inv_hess @ (x * (y - x.T @ beta - b))
     return eic
 
 def compute_loss_grad(x_val, y_val, beta, b):
     "Compute gradient of the validation loss."
-    loss_grad = x_val * (y_val - x_val.T @ beta - b)
+    loss_grad = - x_val * (y_val - x_val.T @ beta - b)
     return loss_grad
 
 def empirical_IF_linear_approx(x, y, x_val, y_val, X, params):
@@ -18,8 +18,8 @@ def empirical_IF_linear_approx(x, y, x_val, y_val, X, params):
     Compute empirical influence functions for model prediction with linear approximation.
     No leave-one-out retraining is needed.
     """
-    beta, b = params
-    eic = empirical_IC_linear_approx(x, y, X, beta, b)
+    beta, b, inv_hess = params
+    eic = empirical_IC_linear_approx(x, y, X, beta, b, inv_hess)
     loss_grad = compute_loss_grad(x_val, y_val, beta, b)
     return loss_grad @ eic
     
