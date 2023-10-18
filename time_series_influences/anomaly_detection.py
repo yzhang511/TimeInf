def1742ed81ce5a44ec3abc90caad0d0a95c670c
Yizi Zhang <yz4123@columbia.edu>
2023-10-18 11:33:11 -0400

    try detection adjustment

--- time_series_influences/anomaly_detection.py
+++ time_series_influences/anomaly_detection.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_recall_fscore_support
 
 def min_max_scaler(x):
     return (x - x.min()) / (x.max() - x.min())
@@ -12,11 +12,37 @@ def scale_influence_functions(influences, block_length):
     scaled_influences[:block_length] = np.nanmean(scaled_influences)
     return np.abs(scaled_influences - np.nanmean(scaled_influences))
 
-def eval_anomaly_detector(ground_truth, model_pred, verbose=True):
+def eval_anomaly_detector(ground_truth, model_pred, verbose=True, adjust_detection=False):
     "Evaluate time series anomaly detectors."
-    prec = precision_score(ground_truth, model_pred)
-    rec = recall_score(ground_truth, model_pred)
-    f1 = f1_score(ground_truth, model_pred)
+    
+    if adjust_detection:
+        anomaly_state = False
+        for i in range(len(ground_truth)):
+            if ground_truth[i] == 1 and model_pred[i] == 1 and not anomaly_state:
+                anomaly_state = True
+                for j in range(i, 0, -1):
+                    if ground_truth[j] == 0:
+                        break
+                    else:
+                        if model_pred[j] == 0:
+                            model_pred[j] = 1
+                for j in range(i, len(ground_truth)):
+                    if ground_truth[j] == 0:
+                        break
+                    else:
+                        if model_pred[j] == 0:
+                            model_pred[j] = 1
+            elif ground_truth[i] == 0:
+                anomaly_state = False
+            if anomaly_state:
+                model_pred[i] = 1       
+        model_pred = np.array(model_pred)
+        ground_truth = np.array(ground_truth)
+
+    prec, rec, f1, support = precision_recall_fscore_support(
+        ground_truth, model_pred, average='binary'
+    )
+        
     if verbose:
         print(f"precision: {prec:.3f} recall: {rec:.3f} F1: {f1:.3f}")
     return prec, rec, f1
